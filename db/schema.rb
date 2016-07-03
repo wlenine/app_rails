fe38960a7ac0b5acc91164f0f7daa5d32ad43757
Lenine <wladmyr.lenine@hotmail.com>
2016-07-03 09:47:46 -0300

    Times

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703122335) do
+ActiveRecord::Schema.define(version: 20160703124515) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160703122335) do
     t.integer  "total_pages"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.string   "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
